b80c24b11f5b400f80a4f1e9928b146ec2fa2f91
Jorgelu1s <mejiajimenez@gmail.com>
2018-03-10 12:09:26 -0500

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
